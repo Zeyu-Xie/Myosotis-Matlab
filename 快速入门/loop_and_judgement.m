dd4812049a9189cc22e5b65b3c2f3e1f9607f424
Acan <xie.zeyu20@gmail.com>
2023-11-14 19:53:05 +0800

    .

--- 快速入门/loop_and_judgement.m
+++ 快速入门/loop_and_judgement.m
@@ -1,3 +1,16 @@
 for i = 3:10
     disp(i)
 end
+
+for i = 1:10
+    a = randi(100)
+
+    if a < 34
+        disp("Low")
+    elseif a < 67
+        disp("Medium")
+    else
+        disp("High")
+    end
+
+end
